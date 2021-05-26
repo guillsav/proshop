42ca9b4654d6abc7b614aca3dd2dbb0a8832512f
guillsav <guillsav@gmail.com>
2021-05-26 15:40:18 -0400

    Auth-service cleanup

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,12 +1,26 @@
 import express from 'express';
 import cookieSession from 'cookie-session';
-import { apiErrorHandler, notFound } from './middlewares';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { apiErrorHandler, notFound, removeHeader } from './middlewares';
 import routes from './routes';
 
 const app = express();
 
 app.set('trust proxy', true);
 app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(removeHeader);
+}
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(
   cookieSession({
     httpOnly: true,
@@ -14,7 +28,6 @@ app.use(
     secure: process.env.NODE_ENV !== 'test'
   })
 );
-
 app.use('/api/v1/auth', routes);
 app.use('*', notFound);
 app.use(apiErrorHandler);
