572f216076ab2625dde998abba767c2d5da64488
guillsav <guillsav@gmail.com>
2021-05-28 10:49:15 -0400

    Added validation helper function to products service

---
+++ products/src/helpers/validationHelper.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request } from 'express';
+import * as Yup from 'yup';
+import { ApiError } from '../controllers';
+
+export async function validate(
+  schema: Yup.ObjectSchema<any>,
+  body: any,
+  req: Request,
+  next: NextFunction
+) {
+  try {
+    const result = await schema.validate(body);
+    if (result) {
+      req.body = result;
+      return next();
+    }
+  } catch ({ errors }) {
+    return next(ApiError.validationError(errors[0]));
+  }
+}
