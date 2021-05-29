7729a520117cda557b4c060d3d33416c16df4a63
guillsav <guillsav@gmail.com>
2021-05-28 20:24:18 -0400

    Minor refactors

--- products/src/app.ts
+++ products/src/app.ts
@@ -1,22 +1,38 @@
 import express from 'express';
 import cookieSession from 'cookie-session';
-import { apiErrorHandler, attachUser, notFound } from './middlewares';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import {
+  apiErrorHandler,
+  attachUser,
+  notFound,
+  removeHeader
+} from './middlewares';
 import routes from './routes';
 
 const app = express();
+const cookieOptions = cookieSession({
+  httpOnly: true,
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+});
 
 app.set('trust proxy', true);
 app.use(express.json());
-app.use(
-  cookieSession({
-    httpOnly: true,
-    signed: false,
-    secure: process.env.NODE_ENV !== 'test'
-  })
-);
+app.use(cors());
 
-app.use(attachUser);
-app.use('/api/v1/products', routes);
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(removeHeader);
+}
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(cookieOptions);
+app.use('/api/v1/products', attachUser, routes);
 app.use('*', notFound);
 app.use(apiErrorHandler);
 
--- products/src/routes/__test__/fetch.test.ts
+++ products/src/routes/__test__/fetch.test.ts
@@ -1,6 +1,5 @@
 import request from 'supertest';
 import app from '../../app';
-import { ProductAttrs } from '../../model';
 
 interface IProduct {
   name: string;
