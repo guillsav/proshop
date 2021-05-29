95b586b84569ec208186d392faea1ec13ae4866f
guillsav <guillsav@gmail.com>
2021-05-28 23:09:18 -0400

    restructured & refactored routes folder in product service

--- products/src/routes/products/__test__/show.test.ts
+++ products/src/routes/products/__test__/show.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
-import app from '../../app';
+import app from '../../../app';
 
 it('returns a 404 not found error if the product is not found', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
---
+++ products/src/routes/products/show.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { ProductsController } from '../../controllers';
+import { asyncHandler } from '../../middlewares';
+
+const router = Router();
+
+/**
+ * @api {GET} /api/v1/products/:id
+ * @apiName View product
+ * @apiGroup Products
+ * @apiSuccess {Number} code HTTP status code from API.
+ * @apiSuccess {Object} Found product.
+ */
+router.get(
+  '/:id',
+  asyncHandler((req: Request, res: Response, next: NextFunction) =>
+    ProductsController.findById(req, res, next)
+  )
+);
+
+export default router;
